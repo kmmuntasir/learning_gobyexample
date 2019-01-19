294925840b78fcf3ca1a9e1a9d67d87c32451231
Muntasir Billah Munna <kmmuntasir@gmail.com>
2019-01-20 01:36:57 +0600

    Read upto switch statement

---
+++ 03_variables.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var declares 1 or more variables.
+
+	var a = "initial"
+	fmt.Println(a)
+
+	// You can declare multiple variables at once.
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	// Go will infer the type of initialized variables.
+
+	var d = true
+	fmt.Println(d)
+
+	// Variables declared without a corresponding initialization are zero-valued. For example, the zero value for an int is 0.
+
+	var e int
+	fmt.Println(e)
+
+	//The := syntax is shorthand for declaring and initializing a variable, e.g. for var f string = "short" in this case.
+
+	f := "short"
+	fmt.Println(f)
+
+	var munna string = "Hello string"
+	fmt.Println(munna)
+
+	var x, y, z = 4, 5, 6
+	fmt.Println(x, y, z)
+}
